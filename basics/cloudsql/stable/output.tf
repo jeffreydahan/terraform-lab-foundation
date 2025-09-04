e2633fb1b9a2f65b6a3dd8af383e423490b5ca99
Your Name <your.email@example.com>
2025-09-04 13:55:34 +0000

    update

--- basics/cloudsql/stable/output.tf
+++ basics/cloudsql/stable/output.tf
@@ -3,16 +3,16 @@
 ## --------------------------------------------------------------
 
 output "cloud_sql_instance" {
-  value       = google_sql_database_instance.mysql
+  value       = google_sql_database_instance.postgres
   description = "Object instance"
 }
 
 output "cloud_sql_private_ip" {
-  value       = google_sql_database_instance.mysql.private_ip_address
+  value       = google_sql_database_instance.postgres.private_ip_address
   description = "Object instance"
 }
 
 output "cloud_sql_public_ip" {
-  value       = google_sql_database_instance.mysql.public_ip_address
+  value       = google_sql_database_instance.postgres.public_ip_address
   description = "Object instance"
 }
